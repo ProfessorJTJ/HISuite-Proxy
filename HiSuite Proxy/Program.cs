9f9cb197b6c18cb5994c652b4233956b62f78c41
JTJ <49002275+ProfessorJTJ@users.noreply.github.com>
2020-06-09 12:45:43 +0430

    New easier patching

--- HiSuite Proxy/Program.cs
+++ HiSuite Proxy/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,11 +12,11 @@ namespace HiSuite_Proxy
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] arguments)
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form1(arguments));
         }
     }
 }
